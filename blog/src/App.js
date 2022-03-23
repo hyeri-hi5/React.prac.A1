96d06f32f0d98914911b1a9e23968e50f50bdd4e
hyeri-hi5 <merryk555@gmail.com>
2022-03-23 23:25:21 +0900

    블로그 기본 구성

---
+++ blog/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let posts = '강남 고기 맛집';
+ 
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div>개발 BLOG</div>
+      </div>
+      <div className='list'>
+        <h3> { posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h3> { posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
